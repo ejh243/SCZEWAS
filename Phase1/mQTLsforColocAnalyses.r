a4f273e4020e70907b85853f36f1b875ca81aac5
ejh243 <e.j.hannon@ex.ac.uk>
2016-07-21 08:56:14 +0100

    Update mQTLsforColocAnalyses.r

--- Phase1/mQTLsforColocAnalyses.r
+++ Phase1/mQTLsforColocAnalyses.r
@@ -1,6 +1,22 @@
 ### Calculate mQTLs for all probes within schizophrenia GWAS regions identified in the PGC GWAS as input for bayesian co-localistaion analysis
 ### Analysis is split by chromosomes
 
+### Copyright (C) 2016  Eilis Hannon
+
+##    This program is free software; you can redistribute it and/or modify
+##   it under the terms of the GNU General Public License as published by
+##    the Free Software Foundation; either version 2 of the License, or
+##    (at your option) any later version.
+
+##    This program is distributed in the hope that it will be useful,
+##    but WITHOUT ANY WARRANTY; without even the implied warranty of
+##    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##    GNU General Public License for more details.
+
+##   You should have received a copy of the GNU General Public License along
+##    with this program; if not, write to the Free Software Foundation, Inc.,
+##    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 setwd("")
 
 library(MatrixEQTL)
